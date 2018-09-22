c0716e9bd687f65c8dde615083d8b01f6edf8e2f
Matt Patrick <hazmat345@gmail.com>
2018-09-22 19:56:33 -0400

    Fixing docstrings

--- brewtils/log/__init__.py
+++ brewtils/log/__init__.py
@@ -4,8 +4,9 @@
 This module is for setting up your plugins logging correctly.
 
 Example:
-    In order to use this, you should simply call ``setup_logger`` in the same file where you
-    initialize your plugin sometime before you initialize your Plugin object.
+    In order to use this, you should simply call ``setup_logger`` in the same
+    file where you initialize your plugin sometime before you initialize your
+    Plugin object.
 
     .. code-block:: python
 
@@ -14,9 +15,20 @@ Example:
         ssl_enabled = False
         system_name = 'my_system'
 
-        setup_logger(bg_host=host, bg_port=port, system_name=system_name, ssl_enabled=ssl_enabled)
-        plugin = Plugin(my_client, bg_host=host, bg_port=port, ssl_enabled=ssl_enabled,
-                        name=system_name, version="0.0.1")
+        setup_logger(
+            bg_host=host,
+            bg_port=port,
+            system_name=system_name,
+            ssl_enabled=ssl_enabled
+        )
+        plugin = Plugin(
+            my_client,
+            bg_host=host,
+            bg_port=port,
+            ssl_enabled=ssl_enabled,
+            name=system_name,
+            version="0.0.1"
+        )
         plugin.run()
 """
 
@@ -33,8 +45,9 @@ DEFAULT_LOGGERS = {
     "yapconf": {"level": "WARN"},
 }
 
-# A simple default format/formatter. Generally speaking, the API should return formatters,
-# but since users can configure their logging, it's better if the formatter has a logical backup.
+# A simple default format/formatter. Generally speaking, the API should return
+# formatters, but since users can configure their logging it's better if the
+# formatter has a logical backup.
 DEFAULT_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 DEFAULT_FORMATTERS = {
     "default": {
@@ -42,8 +55,9 @@ DEFAULT_FORMATTERS = {
     }
 }
 
-# A simple default handler. Generally speaking, the API should return handlers, but since
-# users can configure their logging, it's better if the handler has a logical backup.
+# A simple default handler. Generally speaking, the API should return
+# handlers, but since users can configure their logging it's better if the
+# handler has a logical backup.
 DEFAULT_HANDLERS = {
     "default": {
         "class": "logging.StreamHandler",
@@ -75,52 +89,65 @@ DEFAULT_LOGGING_CONFIG = {
 }
 
 
-def setup_logger(bg_host, bg_port, system_name, ca_cert=None, client_cert=None, ssl_enabled=None):
-    """Configures python logging module to use logging specified in beer-garden API.
+def setup_logger(
+        bg_host, bg_port, system_name,
+        ca_cert=None, client_cert=None, ssl_enabled=None
+):
+    """Sets Python logging to use configuration from Beergarden API
 
-    This method will overwrite your current logging configuration, so only call it if you want
-    beer-garden's logging configuration.
+    This method will overwrite the current logging configuration.
 
-    :param str bg_host: Hostname of a beer-garden
-    :param int bg_port: Port beer-garden is listening on
-    :param str system_name: The system
-    :param ca_cert: Certificate that issued the server certificate used by the beer-garden server
-    :param client_cert: Certificate used by the server making the connection to beer-garden
-    :param bool ssl_enabled: Whether to use SSL for beer-garden communication
-    :return:
+    Args:
+        bg_host (str): Beergarden host
+        bg_port (int): Beergarden port
+        system_name (str): Name of the system
+        ca_cert (str): Path to CA certificate file
+        client_cert (str): Path to client certificate file
+        ssl_enabled (bool): Use SSL when connection to Beergarden
+
+    Returns: None
     """
-    config = get_python_logging_config(bg_host=bg_host, bg_port=bg_port, system_name=system_name,
-                                       ca_cert=ca_cert, client_cert=client_cert,
-                                       ssl_enabled=ssl_enabled)
+    config = get_python_logging_config(
+        bg_host=bg_host, bg_port=bg_port, system_name=system_name,
+        ca_cert=ca_cert, client_cert=client_cert, ssl_enabled=ssl_enabled)
     logging.config.dictConfig(config)
 
 
-def get_python_logging_config(bg_host, bg_port, system_name,
-                              ca_cert=None, client_cert=None, ssl_enabled=None):
-    """Returns a dictionary for the python logging configuration
+def get_python_logging_config(
+        bg_host, bg_port, system_name,
+        ca_cert=None, client_cert=None, ssl_enabled=None
+):
+    """Get Beergarden's logging configuration
 
-    :param str bg_host: Hostname of a beer-garden
-    :param int bg_port: Port beer-garden is listening on
-    :param str system_name: The system
-    :param ca_cert: Certificate that issued the server certificate used by the beer-garden server
-    :param client_cert: Certificate used by the server making the connection to beer-garden
-    :param bool ssl_enabled: Whether to use SSL for beer-garden communication
-    :return: Python logging configuration
+    Args:
+        bg_host (str): Beergarden host
+        bg_port (int): Beergarden port
+        system_name (str): Name of the system
+        ca_cert (str): Path to CA certificate file
+        client_cert (str): Path to client certificate file
+        ssl_enabled (bool): Use SSL when connection to Beergarden
+
+    Returns:
+        dict: The logging configuration for the specified system
     """
-    client = brewtils.get_easy_client(host=bg_host, port=bg_port, ssl_enabled=ssl_enabled,
-                                      ca_cert=ca_cert, client_cert=client_cert)
+    client = brewtils.get_easy_client(
+        host=bg_host, port=bg_port, ssl_enabled=ssl_enabled, ca_cert=ca_cert,
+        client_cert=client_cert
+    )
 
     logging_config = client.get_logging_config(system_name=system_name)
+
     return convert_logging_config(logging_config)
 
 
 def convert_logging_config(logging_config):
-    """Converts a LoggingConfig object into a python logging configuration
+    """Transform a LoggingConfig object into a Python logging configuration
 
-    The python logging configuration that is returned can be passed to `logging.config.dictConfig`
+    Args:
+        logging_config: Beergarden logging config
 
-    :param logging_config:
-    :return: Python logging configuration
+    Returns:
+        dict: The logging configuration
     """
     config_to_return = copy.deepcopy(DEFAULT_PLUGIN_LOGGING_TEMPLATE)
 
@@ -140,4 +167,5 @@ def convert_logging_config(logging_config):
         "level": logging_config.level,
         "handlers": list(config_to_return['handlers'].keys())
     }
+
     return config_to_return

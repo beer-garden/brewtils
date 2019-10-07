110d7f08a709ced2e0491455dfd6d8e53b830229
Matt Patrick <hazmat345@gmail.com>
2019-10-07 11:08:35 -0400

    Adding queues module for backwards compatibility

---
+++ brewtils/queues.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""This module currently exists to maintain backwards compatibility."""
+
+from brewtils.pika import PIKA_ONE, PikaClient
+
+__all__ = ["PIKA_ONE", "PikaClient"]

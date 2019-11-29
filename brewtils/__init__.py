a6c5871a2de5463775962ab47c63ddd7ae68cbcc
Matt Patrick <hazmat345@gmail.com>
2019-11-29 15:46:42 -0500

    Removing deprecated names from brewtils.__all__

--- brewtils/__init__.py
+++ brewtils/__init__.py
@@ -3,7 +3,7 @@ from brewtils.__version__ import __version__
 from brewtils.config import get_argument_parser, get_connection_info, load_config
 from brewtils.decorators import command, parameter, system
 from brewtils.log import configure_logging
-from brewtils.plugin import Plugin, RemotePlugin
+from brewtils.plugin import Plugin, RemotePlugin  # noqa F401
 from brewtils.rest import normalize_url_prefix
 from brewtils.rest.easy_client import get_easy_client, EasyClient
 from brewtils.rest.system_client import SystemClient
@@ -14,14 +14,12 @@ __all__ = [
     "parameter",
     "system",
     "Plugin",
-    "RemotePlugin",
     "EasyClient",
     "SystemClient",
     "get_easy_client",
     "get_argument_parser",
     "get_connection_info",
     "load_config",
-    "get_bg_connection_parameters",
     "configure_logging",
     "normalize_url_prefix",
 ]

42b0f8144ebc841fde342d3ffa3ab03d9621df01
Matt Patrick <hazmat345@gmail.com>
2021-05-04 12:41:22 -0400

    Moving logic around in the chunk resolver

--- brewtils/resolvers/chunks.py
+++ brewtils/resolvers/chunks.py
@@ -18,33 +18,25 @@ class ChunksResolver(ResolverBase):
         self.easy_client = easy_client
 
     def should_upload(self, value, definition=None):
-        return definition and definition.type == "Base64"
+        """
+        Parameter type must be Base64 and the value must be either:
+
+        - String representation of a valid filename.
+        - An IOBase object
+        """
+        if definition and definition.type == "Base64":
+            if sys.version_info[0] == 2:
+                file_types = (io.IOBase, file)  # noqa: F821
+            else:
+                file_types = (io.IOBase,)
+
+            if isinstance(value, six.string_types) or isinstance(value, file_types):
+                return True
+
+        return False
 
     def upload(self, value, definition=None, **kwargs):
-        """Upload the given value to the server if necessary.
-
-        The value can be one of the following:
-
-        1. A string representation of a valid filename.
-        2. An open file descriptor.
-
-        Args:
-            value: Value to upload.
-            definition: Parameter definition
-
-        Returns:
-            A valid beer garden assigned ID
-        """
-        if sys.version_info[0] == 2:
-            file_types = (io.IOBase, file)  # noqa: F821
-        else:
-            file_types = (io.IOBase,)
-        if isinstance(value, six.string_types) or isinstance(value, file_types):
-            return self.easy_client.upload_chunked_file(value, **kwargs)
-        else:
-            raise ValidationError(
-                "Do not know how to upload value of type %s" % type(value)
-            )
+        return self.easy_client.upload_chunked_file(value, **kwargs)
 
     def should_download(self, value, **_):
         if isinstance(value, six.string_types) and UI_FILE_ID_PREFIX in value:
@@ -52,9 +44,4 @@ class ChunksResolver(ResolverBase):
         return False
 
     def download(self, file_id, **_):
-        """Download the given bytes parameter.
-
-        Args:
-            file_id: A BG generated file ID
-        """
         return self.easy_client.download_chunked_file(file_id)

cbb7f8f25d65cf4dbcb0ce823e6a962b7660a93c
Matt Patrick <hazmat345@gmail.com>
2021-05-10 13:36:25 -0400

    Adding type hints to ResolverBase

--- brewtils/resolvers/__init__.py
+++ brewtils/resolvers/__init__.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import abc
+from typing import Any
 
 import six
+
+from brewtils.models import Parameter, Resolvable
 
 
 @six.add_metaclass(abc.ABCMeta)
@@ -10,13 +13,17 @@ class ResolverBase(object):
     """Base for all Resolver implementations"""
 
     def should_upload(self, value, definition):
-        pass
-
-    def should_download(self, value, definition):
+        # type: (Any, Parameter) -> bool
         pass
 
     def upload(self, value, definition):
+        # type: (Any, Parameter) -> Resolvable
+        pass
+
+    def should_download(self, value, definition):
+        # type: (Any, Parameter) -> bool
         pass
 
     def download(self, value, definition):
+        # type: (Resolvable, Parameter) -> Any
         pass

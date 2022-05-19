533e28ddc8b9c88c1c576a1b7c6f0e84ed842649
Virginia White vpwhite <secretsquirrel0516@gmail.com>
2022-05-19 15:54:09 +0000

    More style changes

--- brewtils/decorators.py
+++ brewtils/decorators.py
@@ -125,8 +125,8 @@ def command(
         _deprecate("Use of schema with @command is now deprecated and will eventually be removed")
 
     if template is not None:
-        _deprecate("Use of template with @command is now deprecated" +
-            " and will eventually be removed")
+        _deprecate(
+            "Use of template with @command is now deprecated and will eventually be removed")
 
     if _wrapped is None:
         return functools.partial(

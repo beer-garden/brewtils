6490e63c8e3a403361cfc152aadbc9bd2f1f39f4
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2026-08-27 16:54:39 +0000

    Updating swagger

--- brewtils/swagger_decorator.py
+++ brewtils/swagger_decorator.py
@@ -1,19 +1,15 @@
 import json
 import yaml
-from typing import Any, Callable, Dict, Optional
-from functools import wraps
+from typing import Any, Dict
 from brewtils.models import Parameter, Command
 from brewtils.plugin import (  # noqa F401
     get_current_request_read_only,
 )
-import requests.exceptions
-import urllib3
-from requests import Response, Session  # noqa
-from requests.adapters import HTTPAdapter
-from requests.utils import quote
+from requests import Session  # noqa
 from brewtils.specification import _CONNECTION_SPEC
 from yapconf import YapconfSpec
-import brewtils.plugin
+import re
+
 
 class SwaggerDecorator:
 
@@ -21,8 +17,8 @@ class SwaggerDecorator:
 
     def _parse_swagger_file(self, swagger_path: str) -> Dict[str, Any]:
         """Load and parse a Swagger/OpenAPI file."""
-        with open(swagger_path, 'r') as f:
-            if swagger_path.endswith('.json'):
+        with open(swagger_path, "r") as f:
+            if swagger_path.endswith(".json"):
                 return json.load(f)
             else:
                 return yaml.safe_load(f)
@@ -33,8 +29,14 @@ class SwaggerDecorator:
 
         return response.json()
 
-        
-    def __init__(self, swagger_path: str = None, swagger_url: str = None, base_url: str = None, name=None, version=None):
+    def __init__(
+        self,
+        swagger_path: str = None,
+        swagger_url: str = None,
+        base_url: str = None,
+        name=None,
+        version=None,
+    ):
 
         self._config = self._load_config()
         self.session = Session()
@@ -53,48 +55,142 @@ class SwaggerDecorator:
             self.swagger_spec = self._parse_swagger_url(swagger_url)
         else:
             raise Exception("Unable to get swagger file")
-        
+
+        # Need to handle multiple paths
+        server_param = None
+        self.base_url_final = None
+
         if base_url is None:
-            paths = self.swagger_spec.get('paths', {})
-            self.base_url_final = base_url or self.swagger_spec.get('servers', [{}])[0].get('url', '')
+            servers = self.swagger_spec.get("servers", [])
+
+            if len(servers) == 0:
+                self.base_url_final = ""
+            elif len(servers) == 1:
+                self.base_url_final = servers[0].get("url", "")
+            else:
+                urls = [server.get("url", "") for server in servers]
+                unique_urls = [x for x in set(urls) if x]
+                if len(unique_urls) > 0:
+                    server_param = Parameter(
+                        key="_server",
+                        name="Server URL",
+                        description="Select Target Server",
+                        type="String",
+                        choices=urls,
+                        optional=False,
+                    )
+                else:
+                    raise Exception("Unable to find valid server url")
         else:
             self.base_url_final = base_url
 
         if name:
             self._bg_name = name
-        elif hasattr(self.swagger_spec, 'info') and 'title' in self.swagger_spec['info']:
-            self._bg_name = self.swagger_spec['info']['title']
+        elif (
+            hasattr(self.swagger_spec, "info") and "title" in self.swagger_spec["info"]
+        ):
+            self._bg_name = self.swagger_spec["info"]["title"]
         else:
-            raise ValueError("Swagger spec must have 'info.title' or a name must be provided.")
+            raise ValueError(
+                "Swagger spec must have 'info.title' or a name must be provided."
+            )
 
         if version:
             self._bg_version = version
-        elif hasattr(self.swagger_spec, 'info') and 'version' in self.swagger_spec['info']:
-            self._bg_version = self.swagger_spec['info']['version']
+        elif (
+            hasattr(self.swagger_spec, "info")
+            and "version" in self.swagger_spec["info"]
+        ):
+            self._bg_version = self.swagger_spec["info"]["version"]
         else:
-            raise ValueError("Swagger spec must have 'info.version' or a version must be provided.")
-        
+            raise ValueError(
+                "Swagger spec must have 'info.version' or a version must be provided."
+            )
+
         self._bg_commands = []
         self._current_request = None
         self._commands = []
 
-        paths = self.swagger_spec.get('paths', {})
+        paths = self.swagger_spec.get("paths", {})
         for path, methods in paths.items():
             for method, details in methods.items():
-                if method.lower() not in ['get', 'post', 'put', 'delete', 'patch']:
+                if method.lower() not in ["get", "post", "put", "delete", "patch"]:
                     continue
-                operation_id = details.get('operationId', f"{method}_{path}")
+                operation_id = details.get("operationId", f"{method}_{path}")
                 parameters = []
-                for param in details.get('parameters', []):
-                    parameters.append(self._convert_parameters(param))
-                request_body = details.get('requestBody', {})
+
+                if server_param is not None:
+                    parameters.append(server_param)
+
+                for param in details.get("parameters", []):
+                    new_parameter = self._convert_parameters(param)
+                    if not any(
+                        new_parameter.key == check_parameter.key
+                        for check_parameter in parameters
+                    ):
+                        parameters.append(self._convert_parameters(param))
+                request_body = details.get("requestBody", {})
                 if request_body:
-                    parameters.append(Parameter(key='requestBody', type='Any'))
+                    # Properly parse request body
+                    if not any(
+                        "requestBody" == check_parameter.key
+                        for check_parameter in parameters
+                    ):
+                        parameters.append(
+                            Parameter(key="requestBody", type="Any", optional=False)
+                        )
 
-                description = details.get('description', json.dumps(details))
+                if hasattr(details, "summary"):
+                    description = re.sub(
+                        r"[^\w\s]|\n|\r", "", details.get("summary", "")
+                    )
+                else:
+                    description = re.sub(
+                        r"[^\w\s]|\n|\r", "", details.get("description", "")
+                    )
 
-                self._commands.push(Command(name=operation_id, parameters=parameters, description=description))
+                output_type = None
+
+                tags = details.get("tags", [])
+
+                if hasattr(details, "responses"):
+                    responses = details.get("responses")
+                    for response in ["200", "201"]:
+                        if hasattr(responses, response):
+                            success_response = responses.get(response)
+                            if hasattr(success_response, "content"):
+                                success_content = success_response.get("content")
+                                if hasattr(success_content, "application/json"):
+                                    output_type = "JSON"
+                                    break
+                                if hasattr(success_content, "text/plain"):
+                                    output_type = "STRING"
+                                    break
+                                if hasattr(success_content, "application/xml"):
+                                    output_type = "XML"
+                                    break
+                                if hasattr(success_content, "text/html"):
+                                    output_type = "HTML"
+                                    break
+                                if hasattr(success_content, "text/javascript"):
+                                    output_type = "JS"
+                                    break
+                                if hasattr(success_content, "text/css"):
+                                    output_type = "CSS"
+                                    break
+
+                self._bg_commands.append(
+                    Command(
+                        name=operation_id,
+                        parameters=parameters,
+                        description=description,
+                        output_type=output_type,
+                        tags=tags,
+                    )
+                )
                 setattr(self, operation_id, self._invoke_api)
+
+        self._commands = self._bg_commands
 
     @staticmethod
     def _load_config():
@@ -107,107 +203,210 @@ class SwaggerDecorator:
         Returns:
             The resolved configuration object
         """
-        spec = YapconfSpec(_CONNECTION_SPEC)
+        spec = YapconfSpec(_CONNECTION_SPEC, env_prefix="BG_")
 
-        return spec.load_config('ENVIRONMENT')
+        return spec.load_config("ENVIRONMENT")
 
     def _invoke_api(self, **kwargs):
         current_request = get_current_request_read_only()
         if current_request is None:
-            raise RuntimeError("No current request found. This method must be called within a command execution context.")
+            raise RuntimeError(
+                "No current request found. This method must be called " \
+                "within a command execution context."
+            )
 
-        paths = self.swagger_spec.get('paths', {})
+        paths = self.swagger_spec.get("paths", {})
         for path, methods in paths.items():
             for method, details in methods.items():
-                if method.lower() not in ['get', 'post', 'put', 'delete', 'patch']:
+                if method.lower() not in ["get", "post", "put", "delete", "patch"]:
                     continue
-                if current_request.command == details.get('operationId', f"{method}_{path}"):
+                if current_request.command == details.get(
+                    "operationId", f"{method}_{path}"
+                ):
                     # This is the current API to execute
                     parameters = {}
-                    for param in details.get('parameters', []):
-                        if param.get('name') in kwargs:
-                            parameters[param.get('name')] = kwargs[param.get('name')]
+                    for param in details.get("parameters", []):
+                        if (
+                            param.get("name") in kwargs
+                            and kwargs[param.get("name")] is not None
+                        ):
+                            parameters[param.get("name")] = kwargs[param.get("name")]
 
                     requestBody = None
                     if "requestBody" in kwargs:
                         requestBody = kwargs["requestBody"]
 
-                    url = self.base_url_final + path
+                    if "_server" in kwargs:
+                        target_url = kwargs["_server"]
+                    else:
+                        target_url = self.base_url_final
+
+                    if self._config.ssl_enabled:
+                        url = "https://" + target_url + path
+                    else:
+                        url = "http://" + target_url + path
 
                     # Call Session with detail info
                     if method.lower() == "get":
-                        response =self.session.get(url, params=parameters, json=requestBody)                       
+                        response = self.session.get(
+                            url, params=parameters, json=requestBody
+                        )
                     elif method.lower() == "post":
-                        response =self.session.get(url, params=parameters, json=requestBody)
+                        response = self.session.get(
+                            url, params=parameters, json=requestBody
+                        )
                     elif method.lower() == "put":
-                        response =self.session.put(url, params=parameters, json=requestBody)
+                        response = self.session.put(
+                            url, params=parameters, json=requestBody
+                        )
                     elif method.lower() == "delete":
-                        response =self.session.delete(url, params=parameters, json=requestBody)
+                        response = self.session.delete(
+                            url, params=parameters, json=requestBody
+                        )
                     elif method.lower() == "patch":
-                        response = self.session.patch(url, params=parameters, json=requestBody)
+                        response = self.session.patch(
+                            url, params=parameters, json=requestBody
+                        )
                     else:
-                        raise RuntimeError(f"No matching API found for command {current_request.command}")
+                        raise RuntimeError(
+                            f"No matching API found for command {current_request.command}"
+                        )
 
                     if "application/json" in response.headers.get("Content-Type", ""):
                         return response.json()
                     else:
                         return response.text
-                
-        raise RuntimeError(f"No matching API found for command {current_request.command}")
 
-    def _param_type_to_brewtils(self,items):
-        if hasattr(items, 'anyOf') or hasattr(items, 'oneOf'):
+        raise RuntimeError(
+            f"No matching API found for command {current_request.command}"
+        )
+
+    def _param_type_to_brewtils(self, items):
+        if hasattr(items, "anyOf") or hasattr(items, "oneOf"):
             return "Any"
-        if hasattr(items, '$ref'):
+        if hasattr(items, "$ref"):
             return "Dictionary"
-        
-        swagger_param = items.get('type', None)
-        if swagger_param is None or swagger_param.lower() == 'any':
+
+        swagger_param = items.get("type", None)
+        if swagger_param is None or swagger_param.lower() == "any":
             return "Any"
-        elif swagger_param.lower() == 'integer':
+        elif swagger_param.lower() == "integer":
             return "Integer"
-        elif swagger_param.lower() == 'number':
-            return  "Float"
-        elif swagger_param.lower()  == 'boolean':
+        elif swagger_param.lower() == "number":
+            return "Float"
+        elif swagger_param.lower() == "boolean":
             return "Boolean"
-        elif swagger_param.lower() == 'array':
-            return  "list"
-        elif swagger_param.lower() == 'object':
+        elif swagger_param.lower() == "array":
+            return "list"
+        elif swagger_param.lower() == "object":
             return "Dictionary"
         else:
             return "String"
 
-    def _convert_parameters(self, param):               
+    def _parse_schema(self, schema, parameter):
+        if "type" in schema:
+            if schema["type"] == "array" and "items" in schema:
+                parameter.multiple = True
+                parameter.type = self._param_type_to_brewtils(schema["items"])
+            else:
+                parameter.type = self._param_type_to_brewtils(schema)
+
+        if "minimum" in schema:
+            parameter.minimum = schema["minimum"]
+        if "maximum" in schema:
+            parameter.maximum = schema["maximum"]
+        if "enum" in schema:
+            parameter.choices = schema["enum"]
+
+        parameter.nullable = str(schema.get("nullable", "false")).lower() == "true"
+
+        if "default" in schema:
+            parameter.default = schema["default"]
+
+    def _convert_parameters(self, param):
+
+        if hasattr(param, "$ref"):
+            ref = self._ref_lookup(param.get("$ref"))
+
+            if ref is not None:
+                return self._convert_parameters(ref)
 
         parameter = Parameter()
-     
-        if 'schema' in param:
-            schema = param['schema']
-            if 'type' in schema:
-                if schema['type'] == 'array' and 'items' in schema:
-                    parameter.multiple = True
-                    parameter.type = self._param_type_to_brewtils(schema['items'])
-                else:
-                    parameter.type = self._param_type_to_brewtils(schema)
 
-            if 'minimum' in schema:
-                parameter.minimum = schema['minimum']
-            if 'maximum' in schema:
-                parameter.maximum = schema['maximum']
-            if 'enum' in schema:
-                parameter.choices = schema['enum']
-            
-            parameter.nullable = str(schema.get('nullable', 'false')).lower() == 'true'
+        # Set some defaults that should be overwritten
+        parameter.type = "String"
+        parameter.nullable = True
+        parameter.optional = True
 
-            if 'default' in schema:
-                parameter.default = schema['default']
-            
-        parameter.description = param.get('description', json.dumps(param))
-        parameter.optional = str(param.get('required', 'true')).lower() == 'false'
-        parameter.name = param.get('name')
+        if "schema" in param:
+            schema = param["schema"]
+            self._parse_schema(schema, parameter)
+        else:
+            self._parse_schema(param, parameter)
+
+        parameter.description = re.sub(
+            r"[^\w\s]|\n|\r", "", param.get("description", "")
+        )
+
+        if hasattr(param, "required"):
+            parameter.optional = str(param.get("required", "true")).lower() == "false"
+
+        parameter.name = param.get("name")
+        parameter.key = param.get("name")
         parameter.is_kwarg = True
 
+        if not parameter.nullable and parameter.optional and parameter.default is None:
+            parameter.nullable = True
         return parameter
-    
-    
 
+    def _convert_request_body(self, request_body):
+
+        if hasattr(request_body, "$ref"):
+            ref = self._ref_lookup(request_body.get("$ref"))
+
+            if ref is not None:
+                return self._convert_request_body(ref)
+
+        parameter = Parameter(key="requestBody", type="Any", optional=False)
+        if hasattr(request_body, "description"):
+            parameter.description = re.sub(
+                r"[^\w\s]|\n|\r", "", request_body.get("description", "")
+            )
+
+        if hasattr(request_body, "name"):
+            parameter.display_name = re.sub(
+                r"[^\w\s]|\n|\r", "", request_body.get("name", "")
+            )
+
+        if hasattr(request_body, "content"):
+            content = request_body.get("content")
+            if hasattr(content, "application/json"):
+                parameter.type = "Dictionary"
+
+            if hasattr(content, "text/plain"):
+                parameter.type = "String"
+
+        if (
+            hasattr(request_body, "required")
+            and str(request_body.get("required", "true")).lower() == "false"
+        ):
+            parameter.optional = True
+
+        return parameter
+
+    def _ref_lookup(self, ref: str):
+
+        if not ref.startswith("#/"):
+            return None
+
+        def _path_ref(paths, model):
+            if len(paths) == 0:
+                return model
+
+            key = paths.pop(0)
+            if hasattr(model, key):
+                return _path_ref(paths, getattr(model, key))
+
+            return None
+
+        return _path_ref(ref.split("/")[1:], self.swagger_spec)

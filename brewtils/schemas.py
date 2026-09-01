c08b2caaef3f9dc8706210e4176ad3817b11e0b7
TheBurchLog <5104941+TheBurchLog@users.noreply.github.com>
2026-09-01 09:22:23 -0400

    Add parent_id to request model (#592)
    * Add parent_id to request model
    
    * Update model
    
    * formatting and testing
    
    * Revert formatting
    
    * Update testing
    
    * Expand testing
    
    * Updating Fixtures
    
    * formatting
    
    * Change Log

--- brewtils/schemas.py
+++ brewtils/schemas.py
@@ -387,14 +387,7 @@ class RequestSchema(RequestTemplateSchema):
         lambda: RequestSchema(exclude=("children",)), allow_none=True
     )
     children = fields.List(
-        fields.Nested(
-            lambda: RequestSchema(
-                exclude=(
-                    "parent",
-                    "children",
-                )
-            )
-        ),
+        fields.Nested(lambda: RequestSchema(exclude=("parent",))),
         dump_default=None,
         allow_none=True,
     )
@@ -406,6 +399,7 @@ class RequestSchema(RequestTemplateSchema):
     updated_at = DateTime(allow_none=True, format="epoch")
     status_updated_at = DateTime(allow_none=True, format="epoch")
     has_parent = fields.Bool(allow_none=True)
+    parent_id = fields.Str(allow_none=True)
     requester = fields.String(allow_none=True)
     source_garden = fields.String(allow_none=True)
     target_garden = fields.String(allow_none=True)
